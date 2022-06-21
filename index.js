7593aed9529e0580a51e7d4b7d290a9f276203bc
Hanafiah <hanafiah@cloud-connect.asia>
2022-06-21 19:33:05 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const core = require('@actions/core');
+
+const splaskurl = core.getInput('splaskurl', { required: true });
+const url = core.getInput('url', { required: true });
+const scope = core.getInput('scope', { required: true });
+let payload = {
+    "url" : url,
+    "scope": scope
+}
+let getSplask = axios.post(splaskurl, payload)
+.then(res => {
+    console.log(res)
+})
+.catch(err => {
+    console.error(err); 
+})
